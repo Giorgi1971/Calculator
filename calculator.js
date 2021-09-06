dbd9e1bad7e6fddf39cc702f37faaad2bf8ce6e4
gio1971 <giooorgi7@gmail.com>
2021-09-07 01:15:14 +0400

    added naumbers and +-*/ and work, but not ecxelent

--- calculator.js
+++ calculator.js
@@ -1,1 +1,23 @@
 console.log("Hello in Giorgi's Calculator!")
+
+document.getElementsByTagName("td").innerHTML = "Hello World!";
+
+function clickNum(th){
+    if (document.getElementById("capscreen").textContent == 0){
+        sc1 = th.textContent
+    }else{
+        var sc1 = document.getElementById("capscreen").textContent;
+        sc1 += th.textContent;
+    }
+    document.getElementById("capscreen").innerHTML = sc1;
+}
+
+function clickSum(th) {
+    total = eval(document.getElementById("capscreen").textContent);
+    console.log(total);
+    document.getElementById("capscreen").innerHTML = total;
+}
+
+function click0(th) {
+    document.getElementById("capscreen").innerHTML = 0;
+}
